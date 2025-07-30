8acfdcb9079280c2b44484d1d7bb783c6c06949c
carlos.henrique <carloshenriquesw@gmail.com>
2025-07-30 18:27:12 -0300

    first commit

---
+++ src/main/java/com/java10x/geladeiraMagica/GeladeiraMagicaApplication.java
@@ -0,0 +1,13 @@
+package com.java10x.geladeiraMagica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeladeiraMagicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeladeiraMagicaApplication.class, args);
+	}
+
+}
