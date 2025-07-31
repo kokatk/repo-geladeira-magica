9ae6467ec739f7dbae475a7fcfd82c7420f61714
carlos.henrique <carloshenriquesw@gmail.com>
2025-07-30 22:50:40 -0300

    feat: migration integrado a aplicação com Flyway

--- src/main/java/com/java10x/geladeiraMagica/model/FoodItem.java
+++ src/main/java/com/java10x/geladeiraMagica/model/FoodItem.java
@@ -2,7 +2,6 @@ package com.java10x.geladeiraMagica.model;
 
 import java.time.LocalDate;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
